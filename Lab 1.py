712cf7eda63e4a7ec75bd69c1f4a16e79eacce75
Timofey <medved20032003@gmail.com>
2022-03-16 20:21:13 +0300

    Add constans file

--- Lab 1.py
+++ Lab 1.py
@@ -1,5 +1,6 @@
 import re
-import statistics 
+import statistics
+import constant
 
 
 # ----Main Functions----
@@ -7,32 +8,36 @@ def words_count():
     print("Source text:\n")
     frequency = {}
     document_text = open('text.txt', 'r')
+
     for line_ in document_text:
         print(line_)
+
     document_text = open('text.txt', 'r')
     text_string = document_text.read().lower()
     match_pattern = re.findall(r'\b[a-z]{2,15}\b', text_string)  # use regular names, "\b" check word boundary
+
     for word in match_pattern:
         count = frequency.get(word, 0)  # 0 - default value
         frequency[word] = count + 1
+
     frequency_list = frequency.keys()
+
     for words in frequency_list:
         print(words, "=>", frequency[words])
 
 
 def open_file_and_clean():
     f = open('text.txt', 'r')
-    f = f.read().lower()
-    text = f.replace('?', '.').replace('!', '.').replace('...', '.').replace("Mr.", "mister").replace("Mrs.", "misses")
-    text = text.replace("Dr.", "Doctor").replace("etc.", "etc")
-    return text
+    response = f.read().lower()
 
+    for key, replace_value in constant.REPLACE_DICT.items():
+        for word in response.split():
+            if word == key:
+                response = response.replace(key, replace_value)
 
-def open_file_constants():
-    f = open('input.txt', 'r')
-    n = int(f.readline())
-    k = int(f.readline())
-    return n, k
+    f.close()
+
+    return response
 
 
 def average_count_words():
@@ -50,34 +55,53 @@ def median_count_words():
 def top_k(ngram_dict, k):
     ngram_dict = {k: ngram_dict[k] for k in sorted(ngram_dict, key=ngram_dict.get, reverse=True)}
     tmp_k = 0
+
     print("\nTop", k, "n-gram")
+
     for words in ngram_dict:
         if tmp_k < k:
             print("---- ", words, "=>", ngram_dict[words])
             tmp_k += 1
 
 
+def replace_symbol(text):
+    for key, replace_value in constant.REPLACE_SYMBOL.items():
+        for symbol in text:
+            if symbol == key:
+                text = text.replace(key, replace_value)
+
+    return text
+
+
 def n_gram_search(n, k):
     words_dict = {}
     ngram_dict = {}
+
     print("n=", n, "k=", k)
+
     text = open_file_and_clean()
-    text = text.replace('.', ' ').replace(',', ' ').replace(':', ' ').replace('-', ' ')
-    split_text = text.split()
+
+    response = replace_symbol(text)
+    split_text = response.split()
+
     for word in split_text:
         count = words_dict.get(word, 0)  # 0 - default value
         words_dict[word] = count + 1
+
     for word in words_dict.keys():
         if len(word) >= n:
             tmp_word = word
             n_count = 0
             n_ends = n
+
             for i in range(len(word) - n_ends + 1):
                 ngram = tmp_word[n_count:n_ends]
+
                 if ngram in ngram_dict.keys():  # if ngram already exist in ngram_dict
                     ngram_dict[ngram] += words_dict[word]
                 else:
                     ngram_dict[ngram] = words_dict[word]
+
                 n_count += 1
                 n_ends += 1
     top_k(ngram_dict, k)
@@ -86,8 +110,10 @@ def n_gram_search(n, k):
 def input_n_k():
     print("Do you want to leave the default values of N = 4 and K = 10? (y/n)")
     check = input()
+
     if check == 'y':
-        n, k = open_file_constants()
+        n = constant.N
+        k = constant.K
         n_gram_search(n, k)
     else:
         n = input("Write n: ")
