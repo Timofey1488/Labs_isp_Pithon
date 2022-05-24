a502b4ba9d9ad98938d953d2b31a39e628227a3f
Timofey <medved20032003@gmail.com>
2022-05-25 00:32:31 +0300

    Register troubles solved

--- cryptoshop/urls.py
+++ cryptoshop/urls.py
@@ -6,10 +6,10 @@ from . import views
 
 app_name = 'cryptoshop'
 urlpatterns = [
+    path('register/', views.register, name='register'),
     path('', views.product_list, name='product_list'),
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
-    path('register/', views.register, name='register'),
 ]
 
 if settings.DEBUG:
